d5debea2093db692008ae1a6bfb043d6c57ff1fb
Todokanai <bh951201@gmail.com>
2024-12-25 12:39:12 +0900

    dependency 구조 정리

--- app/src/main/java/com/todokanai/displayalarm/abstracts/AlarmService.kt
+++ app/src/main/java/com/todokanai/displayalarm/abstracts/AlarmService.kt
@@ -1,19 +1,10 @@
 package com.todokanai.displayalarm.abstracts
 
-import android.content.Intent
 import android.net.Uri
-import android.os.Binder
-import android.os.IBinder
 import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.Flow
 
 abstract class AlarmService(): BaseForegroundService() {
-
-    private val binder = Binder()
-
-    override fun onBind(intent:Intent):IBinder{
-        return binder
-    }
 
     override fun onCreate() {
         shouldStartAlarm.asLiveData().observeForever{
