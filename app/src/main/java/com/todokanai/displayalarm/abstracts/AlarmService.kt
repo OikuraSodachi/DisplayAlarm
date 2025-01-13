1e87cc0d8e608211244d53f5f9b2a2bac04973f5
Todokanai <bh951201@gmail.com>
2025-01-13 14:58:49 +0900

    불필요한 layer (BaseAlarmService) 제거

--- app/src/main/java/com/todokanai/displayalarm/abstracts/AlarmService.kt
+++ app/src/main/java/com/todokanai/displayalarm/abstracts/AlarmService.kt
@@ -4,25 +4,57 @@ import android.hardware.display.DisplayManager
 import android.view.Display
 import com.todokanai.displayalarm.objects.Constants.HOUR_MILLI
 import com.todokanai.displayalarm.objects.Constants.MIN_MILLI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import java.util.Calendar
 
-abstract class AlarmService: BaseAlarmService() {
+abstract class AlarmService: BaseForegroundService() {
 
     /** display 상태 instance **/
     private val isDisplayOn = MutableStateFlow(false)
     private val currentTimeFlow = MutableStateFlow(-1L) // 값을 -1로 지정하여, shouldStartAlarm 초기 값 false 만듬
+    open val updatePeriod = 1000L
+
+    private val collector = FlowCollector<Boolean>{
+        if(it){
+            onStartAlarm()
+        }else{
+            onStopAlarm()
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        serviceScope.launch{
+            shouldStartAlarm.collect(collector)
+        }
+        CoroutineScope(Dispatchers.Default).launch {
+            while(true){
+                update()
+                delay(updatePeriod)
+            }
+        }
+    }
+
+
 
     abstract val startTimeFlow: Flow<Long>
     abstract val endTimeFlow: Flow<Long>
     abstract val displayManager:DisplayManager
     abstract val defaultDisplay: Display
 
-    override val shouldStartAlarm by lazy{
+    abstract suspend fun onStartAlarm()
+    abstract suspend fun onStopAlarm()
+
+    val shouldStartAlarm by lazy{
         combine(
             startTimeFlow,
             endTimeFlow,
@@ -38,7 +70,7 @@ abstract class AlarmService: BaseAlarmService() {
     }
 
     /** update values every second **/
-    override suspend fun update() {
+    suspend fun update() {
         when (defaultDisplay.state) {
             1 -> {
                 isDisplayOn.value = false
